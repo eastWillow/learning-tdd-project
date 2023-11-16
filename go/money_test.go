4fb266ec82ba0a7439bdbcec183a0d35f41b0552
eastWillow <eastWillow@users.noreply.github.com>
2023-11-16 13:41:00 +0000

    go pass

--- go/money_test.go
+++ go/money_test.go
@@ -1,15 +1,23 @@
 package main
 
-import(
+import (
 	"testing"
 )
 
-func TestMultiplaction(t *testing.T){
+func TestMultiplaction(t *testing.T) {
 	fiver := Dollar{
-		amount : 5,
+		amount: 5,
 	}
 	tenner := fiver.Times(2)
-	if tenner.amount != 10{
-		t.Errorf("Expected 10, got : [%d]",tenner.amount)
+	if tenner.amount != 10 {
+		t.Errorf("Expected 10, got : [%d]", tenner.amount)
 	}
+}
+
+type Dollar struct {
+	amount int
+}
+
+func (d Dollar) Times(multiplier int) Dollar{
+	return Dollar{10}
 }
