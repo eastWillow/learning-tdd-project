e2440bee5724e5e48f6e0eb9e6dcd4214bc79b35
eastWillow <eastWillow@users.noreply.github.com>
2023-11-21 14:53:02 +0000

    character  2 add money class done

--- cpp/test_money.cpp
+++ cpp/test_money.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <string>
 
 class Dollar{
     public:
@@ -6,7 +7,7 @@ class Dollar{
     Dollar(int amount){
         this->amount = amount;
     }
-    auto times(int multiplier){
+    Dollar times(int multiplier){
         return Dollar(this->amount * multiplier);
     }
 };
@@ -14,7 +15,29 @@ class Dollar{
 auto fiver = Dollar(5);
 auto tenner = fiver.times(2);
 
-TEST(testMath, myTest)
+TEST(TestMoney, TestMultiplication)
 {
     EXPECT_EQ(tenner.amount, 10); 
+}
+
+class Money{
+    public:
+    int amount;
+    std::string currency;
+    Money(int amount, std::string currency){
+        this->amount = amount;
+        this->currency = currency;
+    }
+    Money times(int multiplier){
+        return Money(this->amount * multiplier,"EUR");
+    }
+};
+
+auto tenEuros = Money(10, "EUR");
+auto twentyEuros = tenEuros.times(2);
+
+TEST(TestMoney, TestMultiplicationInEuros)
+{
+    EXPECT_EQ(twentyEuros.amount, 20);
+    EXPECT_EQ(twentyEuros.currency, "EUR");
 }
